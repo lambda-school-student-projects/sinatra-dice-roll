128b1662af100beae94ba2d191d81ae89727e9b2
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-12 02:26:53 +0000

    exercise complete

---
+++ dice.rb
@@ -0,0 +1,74 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'better_errors'
+require 'binding_of_caller'
+
+# configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  erb(:index)
+
+end
+
+get("/dice/2/6") do
+  @rolls = []
+  
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+  erb(:two_six)
+end
+
+get("/dice/2/10") do
+  @rolls = []
+  
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  @rolls = []
+  
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+	
+  erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  @rolls = []
+  
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+  
+  100.times do
+    die = rand(1..6)
+  
+    @rolls.push(die)
+  end
+  
+  erb(:one_hundred_six)
+end
